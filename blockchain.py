576c3fc511ab93f87c51ccc5ea27f8d6f5a941b5
Christopher Barnes <chris.barnes.2000@me.com>
2021-06-10 02:12:58 -0700

    [Update]: Bugs from tutorial

--- blockchain.py
+++ blockchain.py
@@ -8,12 +8,13 @@ class Blockchain(object):
         self.current_transactions = []
         self.new_block(previous_hash=1, proof=100)
 
-    def new_block(self):
+    def new_block(self, proof, previous_hash=None):
         #This function creates new blocks and then adds to the existing chain
         """This method will contain two parameters proof, previous hash"""
         block = {
             'index': len(self.chain) + 1,
             'timestamp' : time(),
+            'transactions': self.current_transactions,
             'proof': proof,
             previous_hash: previous_hash or self.hash(self.chain[-1]),
         }
@@ -21,7 +22,12 @@ class Blockchain(object):
         self.chain.append(block)
         return block
 
-    def new_transaction(self):
+    @property
+    def last_block(self):
+        # Calls and returns the last block of the chain
+        return self.chain[-1]
+
+    def new_transaction(self, sender, recipient, amount):
         #This function adds a new transaction to already existing transactions
         """This will create a new transaction which will be sent to the next block. 
         It will contain three variables including sender, recipient and amount
@@ -34,25 +40,19 @@ class Blockchain(object):
 
         return self.last_block['index'] + 1
 
-
     @staticmethod
-    def hash(block):
+    def hash(self, block):
         #Used for hashing a block
         """The follow code will create a SHA-256 block hash and also ensure that the dictionary is ordered"""
         block_string = json.dumps(block, sort_keys=True).encode()
         return hashlib.sha256(block_string).hexdigest()
 
-    @property
-    def last_block(self):
-        # Calls and returns the last block of the chain
-        return self.chain[-1]
-
-    def proof_of_work(self, last_proof):
+    def proof(self, last_proof):
         """This method is where you the consensus algorithm is implemented.
         It takes two parameters including self and last_proof"""
         proof = 0
         while self.valid_proof(last_proof, proof) is False:
-        proof +=1
+            proof += 1
         return proof
 
     @staticmethod
@@ -61,3 +61,16 @@ class Blockchain(object):
         guess = f'{last_proof}{proof}'.encode()
         guess_hash = hashlib.sha256(guess).hexigest()
         return guess_hash[:4] == "0000"
+
+blockchain = Blockchain()
+t1 = blockchain.new_transaction("Satoshi", "Mike", '5 BTC')
+t2 = blockchain.new_transaction("Mike", "Satoshi", '1 BTC')
+t3 = blockchain.new_transaction("Satoshi", "Hal", '5 BTC')
+blockchain.new_block(blockchain.proof, 12345)
+
+t4 = blockchain.new_transaction("Mike", "Alice", '1 BTC')
+t5 = blockchain.new_transaction("Alice", "Bob", '0.5 BTC')
+t6 = blockchain.new_transaction("Bob", "Mike", '0.5 BTC')
+blockchain.new_block(blockchain.proof, 6789)
+
+print("Genesis block: ", blockchain.chain)
